097d3f567c60a0eff0977d8b60d419e348228f9e
imryan <ryan.cohen@pace.edu>
2016-06-04 18:47:41 -0400

    done

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -4,16 +4,41 @@
 
 @implementation FISAppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    NSDictionary *iceCreams = @{
+                                @"Joe"    : @"Peanut Butter and Chocolate",
+                                @"Tim"    : @"Natural Vanilla",
+                                @"Sophie" : @"Mexican Chocolate",
+                                @"Deniz"  : @"Natural Vanilla",
+                                @"Tom"    : @"Mexican Chocolate",
+                                @"Jim"    : @"Natural Vanilla",
+                                @"Mark"   : @"Cookies 'n Cream",
+                                };
+    
+    NSMutableArray *people = [NSMutableArray array];
+    for (NSString *person in [iceCreams allKeys]) {
+        if ([[iceCreams objectForKey:person] isEqualToString:iceCream]) {
+            [people addObject:person];
+        }
+    }
+    
+    return people;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    NSMutableDictionary *iceCreamCounts = [NSMutableDictionary new];
+    
+    for (NSString *value in [iceCreamByName allValues]) {
+        NSUInteger count = [[self namesForIceCream:value] count];
+        [iceCreamCounts setObject:@(count) forKey:value];
+    }
+    
+    return iceCreamCounts;
+}
 
 @end
